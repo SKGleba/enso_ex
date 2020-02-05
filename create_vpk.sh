4fbeb3d58aca8fa39db5dfa9ff48de57a3fb5b36
SKGleba <30833773+SKGleba@users.noreply.github.com>
2020-02-05 20:13:02 +0100

    added vpk creation script for v4

---
+++ create_vpk.sh
@@ -0,0 +1,20 @@
+(
+cd enso/;
+make FW=360;
+make FW=365;
+cd ../patches/;
+make FW=360;
+make FW=365;
+cd ../installer/;
+cmake ./ && make;
+mv enso_installer.vpk ../enso_ex.vpk;
+rm -rf CMakeFiles && rm cmake_install.cmake && rm CMakeCache.txt && rm Makefile;
+rm -rf emmc_* && rm enso_* && rm kernel2* && rm version.h;
+cd ../patches/;
+rm *.e2xp;
+cd ../enso/;
+rm *.bin;
+echo "";
+echo "DONE!";
+echo "";
+)
